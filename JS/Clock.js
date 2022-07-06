191697cb488e818c7db8f662509fdb804e38254b
sourabhthakur87 <sourabh.thakur_cs20@gla.ac.in>
2022-07-06 15:23:41 +0530

    updation

--- JS/Clock.js
+++ JS/Clock.js
@@ -8,9 +8,9 @@ setInterval(() => {
     let mm = document.getElementById('mm');
     let ss = document.getElementById('ss');
 
-    let hr_dot=document.querySelector('.hr_dot');
-    let min_dot=document.querySelector('.min_dot');
-    let sec_dot=document.querySelector('.sec_dot');
+    let hr_dot = document.querySelector('.hr_dot');
+    let min_dot = document.querySelector('.min_dot');
+    let sec_dot = document.querySelector('.sec_dot');
 
     let h = new Date().getHours();
     let m = new Date().getMinutes();
@@ -27,9 +27,9 @@ setInterval(() => {
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
 
-    hour.innerHTML = h +"<br><span>Hours</span>";
-    Minutes.innerHTML = m+"<br><span>Minutes</span>";
-    Seconds.innerHTML = s+"<br><span>Second</span>";
+    hour.innerHTML = h + "<br><span>Hours</span>";
+    Minutes.innerHTML = m + "<br><span>Minutes</span>";
+    Seconds.innerHTML = s + "<br><span>Second</span>";
     ampm.innerHTML = am;
 
 
@@ -39,9 +39,9 @@ setInterval(() => {
     ss.style.strokeDashoffset = 440 - (440 * s) / 60;
 
 
-    hr_dot.style.transform=`rotate(${h*30}deg)`;
-    min_dot.style.transform=`rotate(${m*6}deg)`;
-    sec_dot.style.transform=`rotate(${s*6}deg)`;
+    hr_dot.style.transform = `rotate(${h * 30}deg)`;
+    min_dot.style.transform = `rotate(${m * 6}deg)`;
+    sec_dot.style.transform = `rotate(${s * 6}deg)`;
 }
 )
 
