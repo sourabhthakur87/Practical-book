3be7303cdd2173e7864408af4f1bd96a9832f89b
sourabhthakur87 <sourabh.thakur_cs20@gla.ac.in>
2022-07-13 17:55:44 +0530

    Update

---
+++ Codes/Login_Form/Firebase/Login.js
@@ -0,0 +1,13 @@
+
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
+
+ const firebaseConfig = {
+   apiKey: "AIzaSyC_UlbUoCVb_zz3Bd_n9C7zUuk0utUTboY",
+   authDomain: "login-page-e8bba.firebaseapp.com",
+   projectId: "login-page-e8bba",
+   storageBucket: "login-page-e8bba.appspot.com",
+   messagingSenderId: "738545466722",
+   appId: "1:738545466722:web:ef3e2bbcee4c11d422803b"
+ };
+
+ const app = initializeApp(firebaseConfig);
---
+++ Codes/Login_Form/Login_Page.js
@@ -0,0 +1,56 @@
+import { getDatabase, get, set, child, update, remove, ref } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-database.js";
+
+var namev, emailv, passv;
+
+const db = getDatabase();
+
+document.getElementById('up').onclick = insertData;
+
+
+var nameb = document.getElementById('name');
+var emailb = document.getElementById('email');
+var passb = document.getElementById('pass');
+
+
+function insertData(e) {
+    e.preventDefault();
+    readFormData();
+    // console.log("Btn Clicked")
+
+    if (namev == "" && emailv == "" && passv == "") {
+        alert("Input Fields cannot be Blank")
+    }
+    else {
+        set(ref(db, "data/" + emailv), {
+            Email: emailv,
+            Name: namev,
+            Pass: passv,
+        }).then(() => {
+            alert("Data Stored Successfully")
+        }).catch((error) => { alert("Unsuccessful", error) });
+    }
+
+
+
+
+    clearFormData();
+}
+
+
+
+
+function readFormData() {
+    namev = nameb.value;
+    emailv = emailb.value;
+    passv = passb.value;
+
+
+    console.log(namev, emailv, passv);
+}
+
+
+function clearFormData() {
+    nameb.value = ""
+    emailb.value = ""
+    passb.value = ""
+}
